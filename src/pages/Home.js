1ec89ba60399e79fe6cff033e342a391891e0005
ProPratik1405 <sariapratik@gmail.com>
2021-08-06 12:34:54 +0530

    Modal added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,100 +1,165 @@
 import {
-    Box,
-    Button,
-    Flex,
-    Tab,
-    TabList,
-    TabPanel,
-    TabPanels,
-    Tabs,
-    Text,
-    useColorMode,
-  } from "@chakra-ui/react";
-  import { useMoralis } from "react-moralis";
-  import NavBar from "../components/NavBar";
-  import DAMTable from "../components/table/Table";
-  
-  export default function Home() {
-  
-    const {Moralis,user,isAuthenticated} = useMoralis();
-  
-    console.log(isAuthenticated,"IS AUHT");
-    // console.log("USER",user.getUsername());
-    // console.log("USERD ",user.attributes)
-  
-    const getBalances = async() => {
-      const options = { chain: 'matic', address: "0x452181dAe31Cf9f42189df71eC64298993BEe6d3" }
-      const balances = await Moralis.Web3.getAllERC20(options);
-      let b = balances[0].balance / 10**18;
-      console.log("BALANCES",balances);
-  }
-  
-  const getTransaction = async() => {
-      const options = { chain: "matic", address: "0x452181dAe31Cf9f42189df71eC64298993BEe6d3", order: "desc" };
-      const transactions = await Moralis.Web3.getTransactions(options);
-      console.log("Transactions",transactions);
-  }
-  
-  // getBalances();
-  // getTransaction();
-    return (
-      // Main Wrapper
-      <Flex direction="column" h="100vh">
-        {/* Wrapper of the main content */}
-        {/* Heading and button */}
-        <NavBar />
-        <Box m="30px 20px 0 20px">
-          <Flex
-            dir="row"
-            justifyContent="space-between"
-            sx={{
-              "@media (max-width: 415px)": {
-                flexDirection: "column",
-              },
+  Box,
+  Button,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  useColorMode,
+  useDisclosure,
+  Stack,
+  Input,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { useMoralis } from "react-moralis";
+import NavBar from "../components/NavBar";
+import DAMTable from "../components/table/Table";
+
+export default function Home() {
+  const { Moralis, user, isAuthenticated } = useMoralis();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  console.log(isAuthenticated, "IS AUHT");
+  // console.log("USER",user.getUsername());
+  // console.log("USERD ",user.attributes)
+
+  const getBalances = async () => {
+    const options = {
+      chain: "matic",
+      address: "0x452181dAe31Cf9f42189df71eC64298993BEe6d3",
+    };
+    const balances = await Moralis.Web3.getAllERC20(options);
+    let b = balances[0].balance / 10 ** 18;
+    console.log("BALANCES", balances);
+  };
+
+  const getTransaction = async () => {
+    const options = {
+      chain: "matic",
+      address: "0x452181dAe31Cf9f42189df71eC64298993BEe6d3",
+      order: "desc",
+    };
+    const transactions = await Moralis.Web3.getTransactions(options);
+    console.log("Transactions", transactions);
+  };
+
+  return (
+    // Main Wrapper
+    <Flex direction="column" h="100vh">
+      {/* Wrapper of the main content */}
+      {/* Heading and button */}
+      <NavBar />
+      <Box m="30px 20px 0 20px">
+        <Flex
+          dir="row"
+          justifyContent="space-between"
+          sx={{
+            "@media (max-width: 415px)": {
+              flexDirection: "column",
+            },
+          }}
+        >
+          <Text fontSize="3xl" fontWeight="700">
+            Browse and Deposit
+          </Text>
+          <Button onClick={getBalances}>Get Balance</Button>
+          <Button onClick={getTransaction}>Get Transaction</Button>
+          <Button
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bgGradient="linear(to-r, cyan.400, blue.500)"
+            onClick={onOpen}
+            _hover={{
+              bgGradient: "linear(to-l, cyan.500, blue.400)",
             }}
           >
-            <Text fontSize="3xl" fontWeight="700">
-              Browse and Deposit
-            </Text>
-            <Button onClick={getBalances}>Get Balance</Button>
-            <Button onClick={getTransaction}>Get Transaction</Button>
-            <Button
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"sm"}
-              fontWeight={600}
-              color={"white"}
-              bgGradient="linear(to-r, cyan.400, blue.500)"
-              href={"#"}
-              _hover={{
-                bgGradient: "linear(to-l, cyan.500, blue.400)",
-              }}
-            >
-              Deposites
-            </Button>
-          </Flex>
-  
-          {/* Tabs for table content */}
-          <Tabs
-            variant="line"
-            mt={30}
-            w="100%"
-            p={3}
-          >
-            <TabList>
-              <Tab>Browse Leaderboard</Tab>
-              <Tab>Browse All</Tab>
-              <Tab>My Deposites</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <DAMTable />
-              </TabPanel>
-              <TabPanel></TabPanel>
-              <TabPanel></TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Box>    
-      </Flex>
-    );
-  }
-  
+            Deposites
+          </Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create Pool</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Stack spacing={3}>
+                  <FormControl >
+                    <FormLabel>Manager</FormLabel>
+                    <Input type="text" />
+                    <FormHelperText>
+                      We'll never share your address.
+                    </FormHelperText>
+                  </FormControl>
+                  <FormControl >
+                    <FormLabel>Name</FormLabel>
+                    <Input type="text" />
+                    <FormHelperText>
+                      We'll never share
+                    </FormHelperText>
+                  </FormControl>
+                  <FormControl >
+                    <FormLabel>Denomination Asset</FormLabel>
+                    <Input type="text" />
+                    <FormHelperText>
+                      We'll never share 
+                    </FormHelperText>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>Denomination Asset Address</FormLabel>
+                    <Input type="text" />
+                    <FormHelperText>
+                      We'll never share
+                    </FormHelperText>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>Comptroller</FormLabel>
+                    <Input type="text" />
+                    <FormHelperText>
+                      We'll never share 
+                    </FormHelperText>
+                  </FormControl>
+                </Stack>
+              </ModalBody>
+              <ModalFooter>
+                <Button variant="ghost">Submit</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Flex>
+        {/* Tabs for table content */}
+        <Tabs variant="line" mt={30} w="100%" p={3}>
+          <TabList>
+            <Tab>Browse Leaderboard</Tab>
+            <Tab>Browse All</Tab>
+            <Tab>My Deposites</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <DAMTable />
+            </TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Flex>
+  );
+}
